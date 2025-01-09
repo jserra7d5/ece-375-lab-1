9ed3c056e7859c712ec27e26fe173c1930be128e
jserra7d5 <progamer5051@gmail.com>
2025-01-08 19:47:35 -0800

    done

--- ece375-L1_skeleton.c
+++ ece375-L1_skeleton.c
@@ -18,11 +18,55 @@ Port D, Pin 4 -> Input -> Right Whisker
 #include <util/delay.h>
 #include <stdio.h>
 
+#define RIGHT_WHISKER (1 << 4) // shift the bit position by 4 to capture the right whisker input bit
+#define LEFT_WHISKER (1 << 5) // shift the bit position by 4 to capture the left whisker input bit
+
 int main(void)
 {
+      // initialize port D for whisker inputs
+      DDRD = 0b00000000; // set all pins as inputs
+      PORTD = 0b11111111; // enable pullup resistors
 
-while (1) // loop forever
+      // initialize port B for motor outputs
+      DDRB = 0b11110000; // port B pin configuration
+      PORTB = 0b11110000; // set the motor outputs to not move initially
+
+      while (1) // loop forever
       {
-	      // Your code goes here
+            uint8_t whisker_state = PIND; // grab the current state of port D's pins
+
+            // checking both whisker inputs, both active low
+            if (!(whisker_state & RIGHT_WHISKER) && !(whisker_state & LEFT_WHISKER)) {
+                  // both whisker inputs are active
+                  PORTB = 0b00000000; // move backward
+                  _delay_ms(1000); // wait for one second
+                  PORTB = 0b10000000;; // turn right
+                  _delay_ms(1000); // wait for one second
+                  PORTB = 0b10010000; // resume moving forward
+            }
+
+            // only check right whisker input, active low
+            else if (!(whisker_state & RIGHT_WHISKER)) {
+                  // right whisker input is active
+                  PORTB = 0b00000000; // move backward
+                  _delay_ms(1000); // wait for one second
+                  PORTB = 0b00010000; // turn left
+                  _delay_ms(1000); // wait for one second
+                  PORTB = 0b10010000; // resume moving forward
+            }
+
+            // only check left whisker input, active low
+            else if (!(whisker_state & LEFT_WHISKER)) {
+                  // left whisker input is active
+                  PORTB = 0b00000000; // move backward
+                  _delay_ms(1000); // wait for one second
+                  PORTB = 0b10000000;; // turn right
+                  _delay_ms(1000); // wait for one second
+                  PORTB = 0b10010000; // resume moving forward
+            }
+
+            else {
+                  PORTB = 0b10010000; // move forward
+            }
       }
 }
